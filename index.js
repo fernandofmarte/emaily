8fb66759b29873a5ca3287cffeae3dbded2dd6b7
Fernando Marte <fernandofmarte@gmail.com>
2019-09-25 16:46:20 -0400

    starting to add in google oauth

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
-app.get('/yo', (req, res) => {
-  res.send({ yo: 'bro' });
-});
-
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 app.get('/dope', (req, res) => {
   res.send({ this: 'is dope' });
 });
+
+passport.use(
+  new GoogleStategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => console.log(accessToken)
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
