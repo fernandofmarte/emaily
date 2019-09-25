3bf1e35db3e718e5e5421063293e23ab1dc72b4b
Fernando Marte <fernandofmarte@gmail.com>
2019-09-25 00:04:05 -0400

    adding dope endpoint

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ app.get('/yo', (req, res) => {
   res.send({ yo: 'bro' });
 });
 
+app.get('/dope', (req, res) => {
+  res.send({ this: 'is dope' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
