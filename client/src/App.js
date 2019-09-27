176f6036478cbeb2399d846e2aacf7f7ab045aee
Fernando Marte <fernandofmarte@gmail.com>
2019-09-26 21:13:56 -0400

    adding concurrently and starting react

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <a className='App-link' href='/auth/google'>
+          click here to sign in through Google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
